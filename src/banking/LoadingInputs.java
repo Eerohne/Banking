975518c58a937878388a22d709982fa36a566bca
cstuser <cstuser@10.1.3.166>
2019-11-25 10:39:35 -0500

    Corrected loading and saving methods

--- src/banking/LoadingInputs.java
+++ src/banking/LoadingInputs.java
@@ -7,17 +7,11 @@ package banking;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-
-
 
 /**
  *
