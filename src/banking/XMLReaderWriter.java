7960c20abd5ab6ecd1c949a372903d44d8aa04ea
cstuser <cstuser@10.1.3.166>
2019-11-28 18:03:49 -0500

    Not finished

--- src/banking/XMLReaderWriter.java
+++ src/banking/XMLReaderWriter.java
@@ -16,17 +16,13 @@ import org.xml.sax.SAXException;
  * @author cstuser
  */
 public class XMLReaderWriter {
-    private static final UserInputManager UIM = new UserInputManager();
-    private static final String PATH = "./src/banking/";
-    private static File file;
+    private static File file = new File("./src/banking/save.xml");;
     
     public static void saveToXML(Bank b) throws FileNotFoundException{
         try {
-            file = new File("./src/banking/" + UIM.retrieveFileName() + ".xml");
             PrintWriter pw = new PrintWriter(file);
             pw.print(b.toXML());
             pw.close();
-            UserInputManager.printError("Successfil save!");
         } catch (Exception e) {
             UserInputManager.printError(e.getMessage());
         }
@@ -35,9 +31,7 @@ public class XMLReaderWriter {
     public static void loadXML(Bank b) throws ParserConfigurationException, IOException, SAXException{
         try {
             b.getClientList().clear();
-            file = new File(PATH + UIM.retrieveFileName() + ".xml");
             b.fromXML();   
-            UserInputManager.printError("Successfil load!");
         } catch (FileNotFoundException e) {
             UserInputManager.printError(e.getMessage());
         }
